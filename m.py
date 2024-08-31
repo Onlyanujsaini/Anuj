b9da0cd0fd7bc2ebdb95c48d31156ea40e0bf3ba
Onlyanujsaini <157143784+Onlyanujsaini@users.noreply.github.com>
2024-08-31 09:38:25 +0530

    Update m.py

--- m.py
+++ m.py
@@ -17,13 +17,13 @@ keep_alive()
 
 # insert your Telegram bot token here
 
-bot = telebot.TeleBot('7543592031:AAHnlEqSQg2MC_Dimz8Hi71iMD8GfYrweTM')
+bot = telebot.TeleBot('7294418593:AAEGEh9ea5XUQ5GHbCPVkXVilMH8oZsRPQ8')
 
 
 
 # Admin user IDs
 
-admin_id = ["6704542925"]
+admin_id = ["817505329"]
 
 
 
